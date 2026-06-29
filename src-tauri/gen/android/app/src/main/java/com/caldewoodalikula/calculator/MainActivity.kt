4fd1c3b8d985b9ed48272bc0ad92965bff1cc96b
coalikula <coalikula@dtbafrica.com>
2026-06-29 16:11:09 +0300

    feat: initialize Tauri application with Leptos integration
    - Added icon for the application.
    - Implemented a Rust command to greet users.
    - Set up main entry point for the Tauri application.
    - Configured Tauri settings in `tauri.conf.json`.
    - Created the main application structure using Leptos.
    - Developed a simple UI for user input and greeting display.
    - Added styles for the application interface.

---
+++ src-tauri/gen/android/app/src/main/java/com/caldewoodalikula/calculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.caldewoodalikula.calculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
